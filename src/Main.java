97a31dd8c7014e8b70d46f2f4f4681d3d1555ac4
PaulaAndrz <pandrzejewska@o2.pl>
2022-03-09 19:41:49 +0100

    add scanner file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię? ");
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String lastName = scanner.nextLine();
+
+        System.out.println( "Cześć " +name + lastName);
+
+
+
+
+
+    }
 }
